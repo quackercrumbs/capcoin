8f78846d24af79784137f36ebc900e32aa387cbb
Jkb6873 <jakub.taraska96@gmail.com>
2018-02-24 23:48:18 -0500

    Transactions partially in place

--- lib/Transaction.h
+++ lib/Transaction.h
@@ -1,11 +1,43 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 
+#include <string>
+#include <vector>
+
+class TxIn{
+public:
+    TxIn    (std::string id, std::string sig, size_t index):
+            id_{id}, sig_{sig}, index_{index}{}
+private:
+    const std::string id_;
+    const std::string sig_;
+    const size_t index_;
+};
+
+class TxOut{
+public:
+    TxOut   (std::string address, double amount):
+            address_{address}, amount_{amount}{}
+private:
+    const std::string address_;
+    const double amount_;
+};
+
+
 class Transaction{
 public:
+    Transaction(std::vector<TxIn>& ins, std::vector<TxOut>& outs){
+        txIns_ = std::move(ins);
+        txOuts_ = std::move(outs);
+        //generate id
+    }
 
 private:
-
+    std::string id_;
+    //TxIn* txIns_;
+    std::vector<TxIn> txIns_;
+    //TxOut* txOuts_;
+    std::vector<TxOut> txOuts_;
 };
 
 #endif
