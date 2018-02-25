df1e576ec08f7d52f9de65b12a4a66412351818c
Jkb6873 <jakub.taraska96@gmail.com>
2018-02-25 00:41:22 -0500

    Worked on Txn id function

--- lib/Transaction.h
+++ lib/Transaction.h
@@ -3,11 +3,18 @@
 
 #include <string>
 #include <vector>
+#include <sstream>
 
 class TxIn{
 public:
     TxIn    (std::string id, std::string sig, size_t index):
             id_{id}, sig_{sig}, index_{index}{}
+
+    std::string GetVal(){
+        std::stringstream ss;
+        ss << id_ << index_;
+        return ss.str();
+    }
 private:
     const std::string id_;
     const std::string sig_;
@@ -18,17 +25,39 @@ class TxOut{
 public:
     TxOut   (std::string address, double amount):
             address_{address}, amount_{amount}{}
+
+    std::string GetVal(){
+        std::stringstream ss;
+        ss << address_ << amount_;
+        return ss.str();
+    }
 private:
     const std::string address_;
     const double amount_;
 };
+
+//std::string TxIn:: GetVal(){
+//    std::stringstream ss;
+//    ss << id_ << index_;
+//    return ss.str();
+//}
+
 
 
 class Transaction{
 public:
     Transaction(std::vector<TxIn>& ins, std::vector<TxOut>& outs):
                 txIns_{std::move(ins)}, txOuts_{std::move(outs)}{
-        //generate id
+        std::string accuInTx = "", accuOutTx = "";
+        //for each transaction in txIns_, append its id and index to accuInTx
+        for (auto i : txIns_)
+            accuInTx = accuInTx + i.GetVal();
+        //for each transaction in txOuts_, append its address and amount to accuOutTx
+        for (auto i : txOuts_)
+            accuOutTx = accuOutTx + i.GetVal();
+        std::hash<std::string> hashifier;
+        //add the two strings and hash them for the id
+        id_ = hashifier(accuInTx + accuOutTx);
     }
 
 private:
