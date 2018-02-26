8ca35511f4caad2311d1c01347d1623ff72f780e
Calvin Quach <calvin@blocpower.org>
2018-02-26 16:29:56 -0500

    Update block currHash variable name to hash_.

--- lib/block.h
+++ lib/block.h
@@ -8,14 +8,14 @@
 class Block {
 public:
     Block   (size_t index, time_t timestamp, size_t difficulty, size_t nonce,
-            std::string currHash, std::string prevHash, Transaction* data);
+            std::string hash, std::string prevHash, Transaction* data);
 
 private:
     const size_t index_;
     const time_t timestamp_;
     const size_t difficulty_;
     const size_t nonce_;
-    const std::string currHash_;
+    const std::string hash_;
     const std::string prevHash_;
     const vector<Transaction> data_;
 };
--- src/block.cpp
+++ src/block.cpp
@@ -1,6 +1,7 @@
 #include "../lib/block.h"
 
+//Todo: use move constructor for data_ 
 Block::Block(size_t index, time_t timestamp, size_t difficulty, size_t nonce,
-	std::string currHash, std::string prevHash, Transaction* data): 
+	std::string hash, std::string prevHash, vector<Transaction> data): 
 	index_{index}, timestamp_{timestamp}, difficulty_{difficulty}, nonce_{nonce},
-	currHash_{currHash}, prevHash_{prevHash}, data_{data} {}
+	hash_{hash}, prevHash_{prevHash}, data_{data} {}
