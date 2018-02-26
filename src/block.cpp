69a20cede4a1c0cefa8cc0d20738e58ab534853b
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-02-26 16:35:05 -0500

    Update block.cpp
    Move constructor for vector

--- src/block.cpp
+++ src/block.cpp
@@ -2,6 +2,6 @@
 
 //Todo: use move constructor for data_ 
 Block::Block(size_t index, time_t timestamp, size_t difficulty, size_t nonce,
-	std::string hash, std::string prevHash, vector<Transaction> data): 
+	std::string hash, std::string prevHash, vector<Transaction>& data): 
 	index_{index}, timestamp_{timestamp}, difficulty_{difficulty}, nonce_{nonce},
-	hash_{hash}, prevHash_{prevHash}, data_{data} {}
+	hash_{hash}, prevHash_{prevHash}, data_{std::move(data)} {}
