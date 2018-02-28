bf27caf03657d7c8bd764e627af6cd840af8f9d5
Jkb6873 <jakub.taraska96@gmail.com>
2018-02-28 04:34:02 -0500

    removed constructor returns

--- src/merkle.cpp
+++ src/merkle.cpp
@@ -14,12 +14,10 @@ MerkleNode<T, hashFunc>::MerkleNode(std::vector<T>& nodes, size_t start, size_t 
         left_   = new MerkleNode(nodes[start]);
         right_  = nullptr;
     }
-    //return new MerkleNode(new MerkleNode<T>(nodes[start]), nullptr);
     if (end - start == 1){
         left_   = new MerkleNode(nodes[start]);
         right_  = new MerkleNode(nodes[end]);
     }
-    //return new MerkleNode(new MerkleNode<T>(nodes[start]), new MerkleNode<T>(nodes[end]));
     size_t mid = ((end - start) / 2) + start;
     left_   = new MerkleNode(nodes, start, mid);
     right_  = new MerkleNode(nodes, mid+1, end);
