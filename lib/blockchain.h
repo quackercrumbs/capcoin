200e3c002b46fb7ebdd50795ac2191ef2145c5f0
calvin <calvinquach@gmail.com>
2018-02-28 09:53:45 -0500

    Change casing of hash methods. Uncomment cpp method and add getlastblock to header

--- lib/blockchain.h
+++ lib/blockchain.h
@@ -11,26 +11,32 @@ class Blockchain {
 public:
 	//Initalize the blockchain by loading data
 	Blockchain ();
+
+    //Returns the current mining difficulty
     size_t GetDifficulty();
 
 	//possibly not necessary, might just ignore this function
 	//Block GetLastBlock();
-
 
     //Imma try to put this into the block class as Validate()
     //If that doesnt work out, ill just uncomment
 	//Checks if the new block is valid by comparing it to the previous block
 	//bool IsValidNewBlock(const Block& newBlock);
 
-    std::string calculateHash(size_t index, std::string prevHash, std::time_t timestamp, std::vector<Transaction>& data, size_t difficulty, size_t nonce);
+    //Calculates the hash of the block's data.
+    std::string CalculateHash(size_t index, std::string prevHash, std::time_t timestamp, std::vector<Transaction>& data, size_t difficulty, size_t nonce);
 
-    bool hashMatchesDifficulty(std::string hash, size_t difficulty);
+    //Checks if the provided hash matches the current mining difficulty
+    bool HashMatchesDifficulty(std::string hash, size_t difficulty);
 
 	//Generates a new block
 	Block GenerateNextBlock();
 
 	//Pushes the new block onto the blockchain
 	bool Push(Block& newBlock);
+
+    //Returns the latest block on the chain
+    Block GetLastBlock();
 
 private:
 	vector<Block> blocks_;
