087c9146c183f8d20fa583218c6a2e9361a5ace3
Jkb6873 <jakub.taraska96@gmail.com>
2018-02-27 21:19:52 -0500

    create validate and getnonce functions, removed nonce from constructor

--- lib/blockchain.h
+++ lib/blockchain.h
@@ -7,9 +7,17 @@ class Blockchain {
 public:
 	//Initalize the blockchain by loading data
 	Blockchain ();
+    size_t GetDifficulty();
 
+	//possibly not necessary, might just ignore this function
+	//Block GetLastBlock();
+
+
+    //Imma try to put this into the block class as Validate()
+    //If that doesnt work out, ill just uncomment
 	//Checks if the new block is valid by comparing it to the previous block
-	bool IsValidNewBlock(const Block& newBlock);
+	//bool IsValidNewBlock(const Block& newBlock);
+
 
 	//Generates a new block
 	Block GenerateNextBlock();
@@ -19,6 +27,7 @@ public:
 
 private:
 	vector<Block> blocks_;
+	friend class Block;
 }
 
 #endif
