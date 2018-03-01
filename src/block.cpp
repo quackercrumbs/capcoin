ea1e5e74b385fc8af75d25ed07a1f0a24326b963
Calvin <calvinquach@gmail.com>
2018-03-01 07:13:44 -0500

    Remove validate from block header and cpp file.

--- src/block.cpp
+++ src/block.cpp
@@ -4,7 +4,3 @@ Block::Block(size_t index, time_t timestamp, size_t difficulty, size_t nonce,
 	std::string hash, std::string prevHash, vector<Transaction>& data): 
 	index_{index}, timestamp_{timestamp}, difficulty_{difficulty}, nonce_{nonce},
 	hash_{hash}, prevHash_{prevHash}, data_{std::move(data)} {}
-
-bool Block::Validate(){
-    return true;
-}
