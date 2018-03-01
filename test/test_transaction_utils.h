21cd583aa5c0af47ed8a48a76f6e5dbe6717113d
Calvin Quach <calvin@blocpower.org>
2018-03-01 14:36:37 -0500

    Move transaction test utils to seperate files.

---
+++ test/test_transaction_utils.h
@@ -0,0 +1,7 @@
+#include "../lib/transaction.h"
+#include "../lib/txin.h"
+#include "../lib/txout.h"
+
+TxIn CreateFakeTxIn();
+TxOut CreateFakeTxOut();
+Transaction CreateFakeTransaction();
--- test/transaction_test.cpp
+++ test/transaction_test.cpp
@@ -4,30 +4,13 @@
 #include "../lib/txin.h"
 #include "../lib/txout.h"
 
+#include "test_transaction_utils.h"
+
 using namespace std;
 
-TxIn CreateFakeTxIn();
-TxOut CreateFakeTxOut();
 
 int main () {
-    TxIn txIn = CreateFakeTxIn();
-    cout << txIn.GetVal() << endl;
-    TxOut txOut = CreateFakeTxOut();
-    cout << txOut.GetVal() << endl;
+    Transaction t = CreateFakeTransaction();
     return 0;
 }
 
-TxIn CreateFakeTxIn() {
-	string id = "12345";
-    string sig = "5677";
-    size_t index = 2;
-    TxIn fakeTxIn{id,sig,index};
-    return fakeTxIn;
-}
-
-TxOut CreateFakeTxOut() {
-	string addr = "";
-	double amt = 20;
-	TxOut fakeTxOut{addr,amt};
-	return fakeTxOut;
-}
