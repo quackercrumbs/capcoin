a4f15d8565478d5f8a7522056b871b4f16fcbdeb
calvin <calvinquach@gmail.com>
2018-03-03 11:52:36 -0500

    Add std namescope to block src file.

--- src/block.cpp
+++ src/block.cpp
@@ -1,6 +1,6 @@
 #include "../lib/block.h"
 
 Block::Block(size_t index, time_t timestamp, size_t difficulty, size_t nonce,
-	std::string hash, std::string prevHash, vector<Transaction>& data): 
+	std::string hash, std::string prevHash, std::vector<Transaction>& data): 
 	index_{index}, timestamp_{timestamp}, difficulty_{difficulty}, nonce_{nonce},
 	hash_{hash}, prevHash_{prevHash}, data_{std::move(data)} {}
