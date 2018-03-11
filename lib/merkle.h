3419206f61d69d6d01cceaec658515c3c61fa18d
calvin <calvinquach@gmail.com>
2018-03-11 10:44:26 -0400

    Add comments to merkle header file.

--- lib/merkle.h
+++ lib/merkle.h
@@ -6,12 +6,23 @@
 
 class MerkleNode {
 public:
+    //Initalize the node with a ptr to the transaction 
     MerkleNode(Transaction &value);
+    //Initalize the left and right node ptrs 
     MerkleNode(MerkleNode *left, MerkleNode *right);
+    //Creates a node with subtrees pointing to transactions 
     MerkleNode(std::vector<Transaction>& nodes);
+
+    //Recursively validates the integreity of the node and its subtrees.
     bool validate() const;
+
+    //Returns the hash of the current node
     const size_t hash() const;
+
+    //Return a ptr to the left subtree
     const MerkleNode* left() const;
+
+    //Return a ptr to the right subtree
     const MerkleNode* right() const;
 private:
     MerkleNode(std::vector<Transaction>& nodes, size_t start, size_t end);
