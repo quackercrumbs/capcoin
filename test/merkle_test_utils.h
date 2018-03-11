f2f14424c9ea30fcbfea0e935c9fdb9e109d116d
calvin <calvinquach@gmail.com>
2018-03-11 14:46:45 -0400

    Edit CreateFakeMerkleTree test and add Compare MerkleTrees test.

--- test/merkle_test_utils.h
+++ test/merkle_test_utils.h
@@ -2,8 +2,24 @@
 #define TEST_MERKLE_UTILS_H
 
 #include "../lib/merkle.h"
+#include "../lib/transaction.h"
 
 //Creates a fake merkle tree root pointer. The tree contains fake/dummy data.
-MerkleNode* CreateFakeMerkleTree();
+MerkleNode* CreateFakeMerkleTree(vector<Transaction> txs);
+
+//Set of test comparing merkle trees
+//Test Cases: same trees and different trees
+bool CompareMerkleTrees();
+
+//Create two hash trees with the same transactions
+//Then compares the hashses at each tree level
+//Return true if the trees match
+bool test_tree_same();
+
+//Creates two trees with different transactions
+//Then compares the hashes at each tree level
+//Return true if the hashes are different
+bool test_tree_different_transaction();
+
 
 #endif
