86fd1043d8511d0fc5898d2ec24dd673e1e0adeb
calvin <calvinquach@gmail.com>
2018-03-11 14:47:04 -0400

    Add implementation code to CompareMerkleTrees.

--- test/merkle_test_utils.cpp
+++ test/merkle_test_utils.cpp
@@ -3,11 +3,50 @@
 
 #include <iostream>
 
-MerkleNode* CreateFakeMerkleTree() {
+MerkleNode* CreateFakeMerkleTree(vector<Transaction> txs) {
 
     Transaction t = CreateFakeTransaction();
     MerkleNode* root = new MerkleNode(t);
     return root;
 }
 
+bool CompareMerkleTrees() {
+    
+    cout << "-- Test: Comparing Merkle Trees --" << endl;
+    bool all_pass = true;
 
+    //Test Case: The trees have the same transaction and same hashes
+    if(!test_tree_same()) {
+        all_pass = false;
+        cout << "FAIL: The trees have same transaction and same hashes" << endl;
+    }
+    
+    //Test Case: The trees have different transactions
+    if(test_tree_different_transaction()) {
+        all_pass = false;
+        cout << "FAIL: The trees have different transactions" << endl;
+    }
+    if(all_pass) {
+        cout << "All test passes!" << endl;
+    }
+    return all_pass;
+}
+
+bool test_tree_same() {
+    bool pass = true;
+
+    MerkleNode* root1 = CreateFakeMerkleTree();
+    MerkleNode* root2 = CreateFakeMerkleTree();
+
+    return pass;
+}
+
+
+bool test_tree_different_transaction() {
+    bool pass = true;
+
+    MerkleNode* root1 = CreateFakeMerkleTree();
+    MerkleNode* root2 = CreateFakeMerkleTree();
+
+    return pass;
+}
