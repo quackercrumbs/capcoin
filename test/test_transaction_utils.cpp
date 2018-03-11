1fd4ea6c67e21ec52f8f8b7a725095fafe348b99
calvin <calvinquach@gmail.com>
2018-03-11 16:54:20 -0400

    Update create fake txin and out to be random. Add create transaction list function. Add random string generator in transaction utils.

--- test/test_transaction_utils.cpp
+++ test/test_transaction_utils.cpp
@@ -1,18 +1,25 @@
 #include "test_transaction_utils.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <algorithm>
+
 using namespace std;
 
 TxIn CreateFakeTxIn() {
-	string id = "12345";
-    string sig = "5677";
-    size_t index = 2;
+    srand(time(NULL));
+	string id = random_string(8);
+    string sig = random_string(8);
+    size_t index = rand() % 1000;
     TxIn fakeTxIn{id,sig,index};
     return fakeTxIn;
 }
 
 TxOut CreateFakeTxOut() {
-	string addr = "";
-	double amt = 20;
+    srand(time(NULL));
+    string addr = random_string(8);
+	double amt = rand() % 1000;
 	TxOut fakeTxOut{addr,amt};
 	return fakeTxOut;
 }
@@ -27,4 +34,29 @@ Transaction CreateFakeTransaction() {
 	//Create fake transaction with fake ins and outs
 	Transaction fakeTransaction{txIns,txOuts};
 	return fakeTransaction;
+}
+
+vector<Transaction> CreateFakeTransactionList() {
+    vector<Transaction> t_list;
+    for(int i = 0; i < 10; i++) {
+        t_list.push_back(CreateFakeTransaction());
+    }
+    return t_list;
+}
+
+
+std::string random_string( size_t length )
+{
+    auto randchar = []() -> char
+    {
+        const char charset[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+        const size_t max_index = (sizeof(charset) - 1);
+        return charset[ rand() % max_index ];
+    };
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
 }
--- test/test_transaction_utils.h
+++ test/test_transaction_utils.h
@@ -10,13 +10,19 @@
 
 
 
-//Creates a fake TxIn with dummy data
+//Creates a fake TxIn with random dummy data
 TxIn CreateFakeTxIn();
 
-//Create a fake TxOut with dummy data
+//Create a fake TxOut with random dummy data
 TxOut CreateFakeTxOut();
 
 //Create a fake transaction with a single fake TxIn and single fake TxOut
 Transaction CreateFakeTransaction();
+
+//Create a list of transactions of random txin and txouts
+std::vector<Transaction> CreateFakeTransactionList();
+
+//Generates a random sequence of characters with the given length
+std::string random_string(size_t length);
 
 #endif
