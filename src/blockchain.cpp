6f47d342b69bcf905b3299fda33cba467cc19db2
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-17 20:32:06 -0400

    GetDifficulty based off of time

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -47,8 +47,15 @@ bool HashMatchesDifficulty(std::string hash, size_t difficulty){
 }
     
 size_t GetDifficulty(){
-    //if the difficulty should not be modified
-    return blocks_[blocks_.size()-1].difficulty_;
+    //if the time between the last two blocks is less than 10 min, increase difficulty
+    if (blocks_[blocks_.size()-1].timestamp_ - blocks_[blocks_.size()-2].timestamp_ < 10)
+        return blocks_[blocks_.size()-1].difficulty_ + 1;
+    //if the time between the last two blocks is more than 15 min, decrease difficulty
+    else if (blocks_[15 < blocks_.size()-1].timestamp_ - blocks_[blocks_.size()-2].timestamp_)
+        return blocks_[blocks_.size()-1].difficulty_ - 1;
+    //if in the 10-15 min range, keep difficulty
+    else
+         return blocks_[blocks_.size()-1].difficulty_;
 }
     
 bool IsValidNewBlock{
