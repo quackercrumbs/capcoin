7671e358bf9b7722ec22d05076c79f1f768831f1
calvin <calvinquach@gmail.com>
2018-03-18 19:18:00 -0400

    Implement all const getter functions for block.

--- src/block.cpp
+++ src/block.cpp
@@ -4,3 +4,31 @@ Block::Block(size_t index, time_t timestamp, size_t difficulty, size_t nonce,
 	std::string hash, std::string prevHash, std::vector<Transaction>& data): 
 	index_{index}, timestamp_{timestamp}, difficulty_{difficulty}, nonce_{nonce},
 	hash_{hash}, prevHash_{prevHash}, data_{std::move(data)} {}
+
+size_t Block::GetIndex() const {
+    return index_;
+}
+
+time_t Block::GetTimestamp() const {
+    return timestamp_;
+}
+
+size_t Block::GetDifficulty() const {
+    return difficulty_;
+}
+
+size_t Block::GetNonce() const {
+    return nonce_;
+}
+
+std::string Block::GetHash() const {
+    return hash_;
+}
+
+std::string Block::GetPreviousHash() const {
+    return prevHash_;
+}
+
+std::vector<Transaction> Block::GetData() const {
+    return data_;
+}
