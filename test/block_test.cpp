9d505100f7af24961ee0b51dfb08c43f798cd970
calvin <calvinquach@gmail.com>
2018-03-18 19:48:04 -0400

    update block test.

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -8,16 +8,24 @@
 #include "../lib/transaction.h"
 #include "../lib/txin.h"
 #include "../lib/txout.h"
+#include "../lib/blockchain.h"
 #include <time.h>
 #include <vector>
 
 #include "test_block_utils.h"
+#include "test_transaction_utils.h"
 
 using namespace std;
 
 int main() {
 	cout << "************Block Class Test************" << endl;
 	
-	Block b = CreateFakeBlock();
+    Blockchain bc;
+    Block genBlock = bc.GetLastBlock();
+    cout << "Hash Match Difficulty: " << bc.HashMatchesDifficulty("0012323123",2) << endl;
 
+    vector<Transaction> t_list = CreateFakeTransactionList(6);
+    Block newBlock = bc.GenerateNextBlock(t_list);     
+    cout << "Gen Block Hash: " << genBlock.GetHash()  << endl;    
+    cout << "New Block Previous Hash: " << newBlock.GetPreviousHash()  << endl;    
 }
