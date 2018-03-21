706543acb9c7f5642fef3ac6c9b91767c3eaebbb
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-21 02:14:35 -0400

    First draft of the TxnPool Sharepool function

---
+++ src/transactionpool.cpp
@@ -0,0 +1,16 @@
+#include "../lib/transactionpool.h"
+
+void TransactionPool:: SharePool(const std::vector<Transactions>& beta){
+  //for every txn in beta
+  for(Transaction x : beta){
+    int i = 0;
+    //search for an identacle txn
+    while(i < pool_.length() && !(pool_[i] == x))i++;
+    if (pool_[i] == x)
+      continue;
+    else{
+      Transaction cpyTxn(x);
+      pool_.push_back(cpyTxn);
+    }
+  }
+}
