6e8ff674357faf10f61ca88f60b97baf28006e25
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-21 02:15:15 -0400

    Update transactionpool.cpp

--- src/transactionpool.cpp
+++ src/transactionpool.cpp
@@ -4,7 +4,7 @@ void TransactionPool:: SharePool(const std::vector<Transactions>& beta){
   //for every txn in beta
   for(Transaction x : beta){
     int i = 0;
-    //search for an identacle txn
+    //search for an identical txn
     while(i < pool_.length() && !(pool_[i] == x))i++;
     if (pool_[i] == x)
       continue;
