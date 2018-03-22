9a6b424726cb003196fda12236d7cec1d47c4533
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-21 22:16:34 -0400

    added forgotten ;

--- src/utxoutpool.cpp
+++ src/utxoutpool.cpp
@@ -1,7 +1,7 @@
 UnspentTxOutPool:: UnspentTxOutPool(){}
 
 TxOut* UnspentTxOutPool:: FindFromIn(const TxIn& input){
-  TxOut* outptr = nullptr
+  TxOut* outptr = nullptr;
   for(UnspentTxOut out: uTxOuts_){
     if (out.txOutId_ == input.id_ && out.txOutIndex_ == input.index_)
       outptr = out;
