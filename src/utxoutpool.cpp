1bcfe524affa3aed0b1e04af69bf699d68bc3849
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-21 22:22:48 -0400

    Change TxOut for UTxOut

--- src/utxoutpool.cpp
+++ src/utxoutpool.cpp
@@ -1,7 +1,7 @@
 UnspentTxOutPool:: UnspentTxOutPool(){}
 
-TxOut* UnspentTxOutPool:: FindFromIn(const TxIn& input){
-  TxOut* outptr = nullptr;
+UnspentTxOut* UnspentTxOutPool:: FindFromIn(const TxIn& input){
+  UnspentTxOut* outptr = nullptr;
   for(UnspentTxOut out: uTxOuts_){
     if (out.txOutId_ == input.id_ && out.txOutIndex_ == input.index_)
       outptr = out;
