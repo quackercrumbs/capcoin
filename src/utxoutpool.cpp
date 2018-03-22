035c30e529644ad8e1db78735da74ffaac9268d7
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-03-21 22:15:50 -0400

    create utxoutpool.cpp

---
+++ src/utxoutpool.cpp
@@ -0,0 +1,11 @@
+UnspentTxOutPool:: UnspentTxOutPool(){}
+
+TxOut* UnspentTxOutPool:: FindFromIn(const TxIn& input){
+  TxOut* outptr = nullptr
+  for(UnspentTxOut out: uTxOuts_){
+    if (out.txOutId_ == input.id_ && out.txOutIndex_ == input.index_)
+      outptr = out;
+  }
+  return outptr;
+}
+
