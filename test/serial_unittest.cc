415fea61359f98c5daec476c7e3d0abca0c807ac
calvinqh <calvinquach@gmail.com>
2018-04-09 17:29:32 -0400

    Add unit test for txin and txout.

--- test/serial_unittest.cc
+++ test/serial_unittest.cc
@@ -15,8 +15,19 @@ TEST(SerialTest,Transaction) {
     EXPECT_FALSE(test_bad_transaction_serial());
 }
 
-TEST(BlockVerificationTest,Invalid) {
-    
+TEST(SerialTest,TxIn) {
+    EXPECT_TRUE(test_good_txin_serial());
+    EXPECT_FALSE(test_bad_txin_serial());   
+}
+
+TEST(SerialTest,TxOut) {
+    EXPECT_TRUE(test_good_txout_serial());
+    EXPECT_FALSE(test_bad_txout_serial());
+}
+
+TEST(SerialTest,Block) {
+    //EXPECT_TRUE(test_good_block_serial());
+    //EXPECT_FALSE(test_bad_block_serial());
 }
 
 }
