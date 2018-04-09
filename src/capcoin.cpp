21376976cb0a79901bed8e8f61919c3d1e63ff76
David-Bohl <davebohl@gmail.com>
2018-04-09 15:23:17 -0400

    ui run moved to main block for testing

--- src/capcoin.cpp
+++ src/capcoin.cpp
@@ -18,8 +18,6 @@ int main(int argc, char *argv[]) {
     UserInterface ui;
     ui.welcome();
 
-
-
     //create network, connect as server or client
     Network network;
     if( (argc > 1) && (strncmp (argv[1], "server", 6) == 0) ){
@@ -29,8 +27,39 @@ int main(int argc, char *argv[]) {
       // network.startClient();
     }
 
+    //ui.run();
 
-    ui.run();
+    while(1){
+      std::cout << std::endl;
+      std::cout << "O - Overview     S - Send     R - Receive     T - Transactions     H - Help     C - Chat (for testing)" << std::endl;
+      std::cout << "please make a selection" << std::endl;
+      std::string selection;
+      std::cin >> selection;
+
+      if(selection == "O" || selection == "o" ){
+        std::cout << "run overview" << std::endl;
+      }
+      else if(selection == "S" || selection == "s" ){
+        std::cout << "run send" << std::endl;
+      }
+      else if(selection == "R" || selection == "r" ){
+        std::cout << "run receive" << std::endl;
+      }
+      else if(selection == "T" || selection == "t" ){
+        std::cout << "run transactions" << std::endl;
+      }
+      else if(selection == "H" || selection == "h" ){
+        std::cout << "run help" << std::endl;
+      }
+      else if(selection == "C" || selection == "c" ){
+        network.startClient();
+      }
+      else{
+        std::cout << "input not valid" << std::endl;
+      }
+    }
+
+
 
 
     return 0;
