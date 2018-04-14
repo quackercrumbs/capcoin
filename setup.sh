c392d44c52f03d1a6b1ff2d873498a795f0b3200
calvinqh <calvinquach@gmail.com>
2018-04-14 16:59:59 -0400

    Update setup to use make file in test.

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ if [ ! -d "test/googletest" ]; then
     git clone git@github.com:google/googletest test/googletest
 fi
 if [ -d "test/googletest" ]; then
-    make -C test/googletest/googletest/make all
+    make -C test/ all
 fi
