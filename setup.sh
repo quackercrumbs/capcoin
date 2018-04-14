c1d07227ae96d3d344ebfe42ebe55dffb9383f2d
calvinqh <calvinquach@gmail.com>
2018-04-14 16:47:45 -0400

    Add setup script.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-if [ ! -d test/googletests ]; then
+if [ ! -d "test/googletest" ]; then
     git clone git@github.com:google/googletest test/googletest
+fi
+if [ -d "test/googletest" ]; then
+    make -C test/googletest/googletest/make all
 fi
