ff11728aa9334e482218269b6c46eb619d80685c
calvin <calvinquach@gmail.com>
2018-04-20 12:51:01 -0400

    Implement utxout getter functions.

--- src/utxout.cpp
+++ src/utxout.cpp
@@ -1,4 +1,19 @@
 #include "utxout.h"
 
-UnspentTxOut::UnspentTxOut(std::string txOutId, std::string address, size_t txOutIndex, size_t amount):
-                txOutId_{txOutId}, address_{address}, txOutIndex_{txOutIndex}, amount_{amount}{}
+UnspentTxOut::UnspentTxOut(std::string txOutId, std::string address, size_t txOutIndex, size_t amount): txOutId_{txOutId}, address_{address}, txOutIndex_{txOutIndex}, amount_{amount}{}
+
+std::string UnspentTxOut::GetId() {
+    return txOutId_;
+}
+
+std::string UnspentTxOut::GetAddress() {
+    return address_;
+}
+
+size_t UnspentTxOut::GetIndex() {
+    return txOutIndex_;
+}
+
+size_t UnspentTxOut::GetAmount() {
+    return amount_;
+}
