5394608c9329f499420d90511c7f7278322dd55b
Frederick Erlikh <erlikhf@gmail.com>
2018-04-23 18:23:52 -0400

    Restored serialize.h and cpp

--- lib/serialize.h
+++ lib/serialize.h
@@ -4,14 +4,25 @@
 #include "transaction.h"
 #include "block.h"
 #include "blockchain.h"
+#include "utxout.h"
+#include <stdlib.h>
 
 class Serialize{
 public:
-  Serialize(Transaction& toBeSent);
-  Serialize(Block& toBeSent);
+  void operator()(Transaction& toBeSent);
+  void operator()(Block& toBeSent);
+  void operator()(UnspentTxOut& toBeSent);
+
+  void operator()(TxIn& toBeSent);
+  void operator()(TxOut& toBeSent);
   std::string toString();
 private:
   std::string output;
 };
+TxIn JSONtoTxIn(std::string instring);
+TxOut JSONtoTxOut(std::string outstring);
+Transaction JSONtoTx(std::string txnString);
+UnspentTxOut JSONtoUTxO(std::string blockString);
+Block JSONtoBlock(std::string blockString);
 
 #endif
--- src/serialize.cpp
+++ src/serialize.cpp
@@ -1,23 +1,25 @@
 #include "serialize.h"
+#include <iostream>
 
-Serialize::Serialize(Transaction& toBeSent){
+void Serialize::operator()(Transaction& toBeSent){
   std::stringstream accumulator;
+  Serialize smallSerial;
   accumulator << "\"TRANSACTION\":{\"ID\":\"" << toBeSent.hash() << "\"";
   for(TxIn in: toBeSent.GetTxIns()){
-    accumulator << ",\"IN\":{\"ID\":\"" << in.GetId() << "\","
-      << "\"SIG\":\"" << in.GetSignature() << "\","
-      << "\"INDEX\":\"" << in.GetIndex() << "\"}";
+    smallSerial(in);
+    accumulator << smallSerial.toString();
   }
   for(TxOut out: toBeSent.GetTxOuts()){
-    accumulator << ",\"OUT\":{\"AMOUNT\":\"" << out.GetAmount() << "\","
-      << "\"ADDRESS\":\"" << out.GetAddress() << "\"}";
+    smallSerial(out);
+    accumulator << smallSerial.toString();
   }
   accumulator << "}";
   accumulator >> output;
 }
 
-Serialize::Serialize(Block& toBeSent){
+void Serialize::operator()(Block& toBeSent){
   std::stringstream accumulator;
+  Serialize smallSerial;
   accumulator << "\"BLOCK\":{\"INDEX\":\"" << toBeSent.GetIndex() << "\","
     << "\"TIMESTAMP\":\"" << toBeSent.GetTimestamp() << "\","
     << "\"DIFFICULTY\":\"" << toBeSent.GetDifficulty() << "\","
@@ -25,13 +27,172 @@ Serialize::Serialize(Block& toBeSent){
     << "\"HASH\":\"" << toBeSent.GetHash() << "\","
     << "\"PREVHASH\":\"" << toBeSent.GetPreviousHash() << "\"";
   for (Transaction tx: toBeSent.GetData()){
-    Serialize txSerial(tx);
-    accumulator << "," << txSerial.toString();
+    smallSerial(tx);
+    accumulator << "," << smallSerial.toString();
   }
   accumulator << "}";
+  accumulator >> output;
+}
+void Serialize::operator()(TxIn& toBeSent){
+  std::stringstream accumulator;
+  accumulator << ",\"IN\":{\"ID\":\"" << toBeSent.GetId() << "\","
+    << "\"SIG\":\"" << toBeSent.GetSignature() << "\","
+    << "\"INDEX\":\"" << toBeSent.GetIndex() << "\"}";
+  accumulator >> output;
+}
+void Serialize::operator()(TxOut& toBeSent){
+  std::stringstream accumulator;
+  accumulator << ",\"OUT\":{\"AMOUNT\":\"" << toBeSent.GetAmount() << "\","
+    << "\"ADDRESS\":\"" << toBeSent.GetAddress() << "\"}";
+  accumulator >> output;
+}
+
+void Serialize::operator()(UnspentTxOut& toBeSent){
+  std::stringstream accumulator;
+  accumulator << "\"UNSPENTTXOUT\":{\"ID\":\"" << toBeSent.GetId() << "\","
+    << "\"ADDRESS\":\"" << toBeSent.GetAddress() << "\","
+    << "\"INDEX\":\"" << toBeSent.GetIndex() << "\","
+    << "\"AMOUNT\":\"" << toBeSent.GetAmount() << "\"}";
   accumulator >> output;
 }
 
 std::string Serialize::toString(){
   return output;
+}
+
+Block JSONtoBlock(std::string blockString){
+    size_t index, difficulty, nonce, start = 19, end = 19;
+    time_t timestamp;
+    std::string prevHash, hash;
+    std::vector<Transaction> data;
+    while (blockString[end] != '\"'){end++;}
+    index = strtol(blockString.substr(start, end-start).c_str(), NULL, 10);
+    start = end + 15;
+    end = start;
+    while (blockString[end] != '\"'){end++;}
+    timestamp = strtol((blockString.substr(start, end-start).c_str()), NULL, 10);
+    start = end + 16;
+    end = start;
+    while (blockString[end] != '\"'){end++;}
+    difficulty = strtol((blockString.substr(start, end-start).c_str()), NULL, 10);
+    start = end + 11;
+    end = start;
+    while (blockString[end] != '\"'){end++;}
+    nonce = strtol((blockString.substr(start, end-start).c_str()), NULL, 10);
+    start = end + 10;
+    end = start;
+    while (blockString[end] != '\"'){end++;}
+    hash = blockString.substr(start, end-start);
+    start = end + 14;
+    end = start;
+    while (blockString[end] != '\"'){end++;}
+    prevHash = blockString.substr(start, end-start);
+    start = end + 2;
+    end = start + 11;
+    std::string prefix = blockString.substr(start+1, end-start);
+    while(prefix == "TRANSACTION"){
+        end = start + 16;
+        size_t brackets = 1;
+        while (brackets != 0 && !(blockString.length() < end)){
+            if(blockString[end] == '{')
+                brackets++;
+            if(blockString[end] == '}')
+                brackets--;
+            end++;
+        }
+        Transaction tx = JSONtoTx(blockString.substr(start, end));
+        data.push_back(tx);
+        start = end + 1;
+        end = start + 11;
+        if (end > blockString.length())
+            break;
+        prefix = blockString.substr(start+1, end-start);
+    }
+    Block output(index, timestamp, difficulty, nonce, hash, prevHash, data);
+    return output;
+}
+
+Transaction JSONtoTx(std::string txnString){
+    std::string id, prefix;
+    std::vector<TxIn> ins;
+    std::vector<TxOut> outs;
+    size_t start = 21, end = 21;
+    while (txnString[end] != '\"'){end++;}
+    id = txnString.substr(start, end-start);
+    start = end + 2;
+    end = start + 2;
+    prefix = txnString.substr(start+1, end-start);
+    while(prefix == "IN"){
+        while(txnString[end] != '}'){end++;}
+        TxIn in = JSONtoTxIn(txnString.substr(start, end-start));
+        ins.push_back(in);
+        start = end + 2;
+        end = start + 2;
+        if (end > txnString.length())
+            break;
+        prefix = txnString.substr(start+1, end-start);
+    }
+    prefix = txnString.substr(start+1, (++end)-start);
+    while(prefix == "OUT"){
+        while(txnString[end] != '}'){end++;}
+        TxOut out = JSONtoTxOut(txnString.substr(start, end-start));
+        outs.push_back(out);
+        start = end + 2;
+        end = start + 3;
+        if (end > txnString.length())
+            break;
+        prefix = txnString.substr(start+1, end-start);
+    }
+    Transaction output(ins, outs);
+    return output;
+}
+
+TxIn JSONtoTxIn(std::string instring){
+    std::string id;
+    std::string sig;
+    size_t index, start = 12, end = 12;
+    while (instring[end] != '\"'){end++;}
+    id = instring.substr(start, end-start);
+    start = end + 9;
+    end = start;
+    while (instring[end] != '\"'){end++;}
+    sig = instring.substr(start, end-start);
+    start = end + 11;
+    index = strtol((instring.c_str() + start), NULL, 10);
+    TxIn output(id, sig, index);
+    return output;
+}
+
+TxOut JSONtoTxOut(std::string outstring){
+    std::string address;
+    double amount;
+    size_t start = 17, end = 17;
+    while (outstring[end] != '\"'){end++;}
+    amount = strtod(outstring.substr(start, end-start).c_str(), NULL);
+    start = end + 13;
+    end = start;
+    while (outstring[end] != '\"'){end++;}
+    address = outstring.substr(start, end-start);
+    TxOut output(address, amount);
+    return output;
+}
+
+UnspentTxOut JSONtoUTxO(std::string outstring){
+    std::string txOutId, address;
+    size_t txOutIndex, start = 22, end = 22;
+    double amount;
+    while (outstring[end] != '\"'){end++;}
+    txOutId = outstring.substr(start, end-start);
+    start = end + 13;
+    end = start;
+    while (outstring[end] != '\"'){end++;}
+    address = outstring.substr(start, end-start);
+    start = end + 11;
+    end = start;
+    while (outstring[end] != '\"'){end++;}
+    txOutIndex = strtol((outstring.substr(start, end-start).c_str()), NULL, 10);
+    start = end + 12;
+    amount = strtod((outstring.c_str() + start), NULL);
+    UnspentTxOut output(txOutId, address, txOutIndex, amount);
+    return output;
 }
