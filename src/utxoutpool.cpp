10fdc4e8eb83e2cab6dae5a7861e010215bf343d
David-Bohl <davebohl@gmail.com>
2018-04-25 19:41:01 -0400

    DOES  NOT COMPILE git add .! Work on restoring utxoutpool

--- src/utxoutpool.cpp
+++ src/utxoutpool.cpp
@@ -1,5 +1,23 @@
 #include "utxoutpool.h"
 
-UnspentTxOutPool::UnspentTxOutPool() {
+UnspentTxOutPool:: UnspentTxOutPool(){}
 
+UnspentTxOut* UnspentTxOutPool:: FindFromIn(const TxIn& input) const{
+  UnspentTxOut* outptr = nullptr;
+  for(UnspentTxOut out: uTxOuts_){
+    std::string x, y;
+    x = out.txOutId_;
+    y = input.GetId();
+    size_t a = out.txOutIndex_, b = input.GetIndex();
+    if (out.txOutId_ == input.GetId() && out.txOutIndex_ == input.GetIndex()){
+      outptr = &out;
+      break;
+    }
+  }
+  return outptr;
+}
+
+bool UnspentTxOutPool:: AddTxn(UnspentTxOut& input){
+  uTxOuts_.push_back(input);
+  return true;
 }
