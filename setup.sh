8e269a572e60ec11093a4eabd7cf747ccb77e02c
calvinqh <calvinquach@gmail.com>
2018-04-27 09:17:28 -0400

    Update setup boost and breep script.

--- setup.sh
+++ setup.sh
@@ -1,27 +1,28 @@
-#Install and build Breep if it is not found in modules
-if [ ! -d "modules/Breep" ]; then
-    echo "Enter any key to install Breep...."
-    read user_input
-    git clone git@github.com:Organic-Code/Breep.git modules/Breep
-    cd modules/Breep
-    cmake .
-    cd ../../ #return to project root dir
-fi
-
-
+# Check if Boost folder exist in /usr/local/include
 if [ -d  "/usr/local/include/boost" ]; then
     echo "Boost found."
 fi
 
-#Install and build boost if it is not found in user/local/include (headers not found)
+# If boost is not found in usr/local/include (headers not found), then we clone boost and install it there
 if [ ! -d "/usr/local/include/boost" ]; then
     echo "Enter any key to install boost ...."
     read user_input
-    git clone --recursive git@github.com:boostorg/boost.git modules/boost
+    git clone --recursive https://github.com/boostorg/boost modules/boost
     cd modules/boost
     #Header files are saved in /usr/local/include
     #Libs will be saved in /usr/local/lib/
     sudo ./bootstrap.sh --prefix=/usr/local 
     sudo ./b2 install                       
+    cd ../../ #return to project root dir
+fi
+
+# If Breep isn't modules, then clone the repo a build
+# Note, if the repo is already there, it will assume that the project was built
+if [ ! -d "modules/Breep" ]; then
+    echo "Enter any key to install Breep...."
+    read user_input
+    git clone https://github.com/Organic-Code/Breep modules/Breep
+    cd modules/Breep
+    cmake .
     cd ../../ #return to project root dir
 fi
