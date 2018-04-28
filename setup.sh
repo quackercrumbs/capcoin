843c51b8428f1a11e4af1a962ab1fdecf47cfd13
calvinqh <calvinquach@gmail.com>
2018-04-28 17:21:58 -0400

    Remove boost clone and build from setup script.

--- setup.sh
+++ setup.sh
@@ -3,22 +3,6 @@ if [ -d  "/usr/local/include/boost" ]; then
     echo "Boost found."
 fi
 
-# If boost is not found in usr/local/include (headers not found), then we clone boost and install it there
-if [ ! -d "/usr/local/include/boost" ]; then
-    echo "Press Enter to install boost ...."
-    read user_input
-    git clone --recursive https://github.com/boostorg/boost modules/boost
-    echo "Boost clone process complete."
-    echo "Press Enter to proceed to build process...."
-    read user_input
-    cd modules/boost
-    #Header files are saved in /usr/local/include
-    #Libs will be saved in /usr/local/lib/
-    sudo ./bootstrap.sh --prefix=/usr/local 
-    sudo ./b2 install                       
-    cd ../../ #return to project root dir
-fi
-
 # If Breep isn't modules, then clone the repo a build
 # Note, if the repo is already there, it will assume that the project was built
 if [ ! -d "modules/Breep" ]; then
