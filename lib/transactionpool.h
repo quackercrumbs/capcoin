39e1d172818914e5bcb9244a69fbcaae18494113
calvinqh <calvinquach@gmail.com>
2018-04-28 11:01:53 -0400

    Add addTxn to txpool class.

--- lib/transactionpool.h
+++ lib/transactionpool.h
@@ -2,12 +2,19 @@
 #define TRANSACTIONPOOL_H
 
 #include <queue>
+#include <iostream>
+
 #include "transaction.h"
 
 class TransactionPool {
 public:
 	//Initialize the transaction pool (loads data from pool)
 	TransactionPool();
+    
+    //Pushes the newTx into the memory pool
+    bool AddTransaction(Transaction newTx);
+
+    friend std::ostream& operator<<(std::ostream& os, const TransactionPool& txpool);
 
 private:
 
