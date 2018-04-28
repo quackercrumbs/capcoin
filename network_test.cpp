4b596013627215379409c5c982409c1f19caf253
calvinqh <calvinquach@gmail.com>
2018-04-28 19:31:45 -0400

    Remove bin file and add std out lines.

--- network_test.cpp
+++ network_test.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
     
 
     std::cout << "====================================" << std::endl;
-    std::cout << "Listening from port " << p << std::endl;
+    std::cout << "You're listening from port " << p << std::endl;
     std::cout << "====================================" << std::endl;
    
     Blockchain bc;
@@ -22,36 +22,37 @@ int main(int argc, char* argv[]) {
     //If the node is hosting (aka the inital node)  THIS IS REQUIRED
     if (argc == 3) {
         if(strcmp(argv[2],"initial") == 0) {
-            std::cout << "Initial Node" << std::endl;
+            std::cout << "Your are running as the initial node" << std::endl;
             net.run();
         }
     }
     std::string ans = "";
-        while(true) {
+    std::cout << "Enter !h to display options" << std::endl;
+    while(true) {
+        std::getline(std::cin, ans);
+        if(ans == "!q") {
+           std::cout << "Leaving..." << std::endl;
+           break;
+        } 
+        else if(ans == "!connect") {
+            std::cout << "Enter node ip: " << std::endl;
+            std::string new_address; 
+            std::getline(std::cin, new_address);
+            
+            std::cout << "Enter node port: " << std::endl;
             std::getline(std::cin, ans);
-            if(ans == "!q") {
-               std::cout << "Leaving..." << std::endl;
-               break;
-            } 
-            else if(ans == "!connect") {
-                std::cout << "Enter node ip: " << std::endl;
-                std::string new_address; 
-                std::getline(std::cin, new_address);
-                
-                std::cout << "Enter node port: " << std::endl;
-                std::getline(std::cin, ans);
-                unsigned short new_port = atoi(ans.c_str());
+            unsigned short new_port = atoi(ans.c_str());
 
-                net.AddPeer(new_address, new_port);
-            }
-            else if(ans == "!help") {
-                std::cout << "!connect, form to connect to node" << std::endl;
-                std::cout << "!q, exit node" << std::endl;
-                std::cout << "!help, help menu" << std::endl;
-            }
-            else {
-                net.broadcastMessage(ans);
-            }
+            net.AddPeer(new_address, new_port);
+        }
+        else if(ans == "!help") {
+            std::cout << "!connect, form to connect to node" << std::endl;
+            std::cout << "!q, exit node" << std::endl;
+            std::cout << "!help, help menu" << std::endl;
+        }
+        else {
+            net.broadcastMessage(ans);
+        }
     }
     net.Close();
 }
