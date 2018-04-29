a5fc18a78b85516cc352915ec2923e63840370ec
hugo001 <contacthugo1@gmail.com>
2018-04-29 09:53:37 -0400

    temporary ambiguity fix

--- src/txin.cpp
+++ src/txin.cpp
@@ -3,7 +3,7 @@
 TxIn::TxIn (std::string id, std::string sig, size_t index):
             id_{id}, sig_{sig}, index_{index}{}
 
-std::string TxIn:: GetVal()const{
+std::string TxIn::toString(){
     std::stringstream ss;
     ss << id_ << index_;
     return ss.str();
--- src/txout.cpp
+++ src/txout.cpp
@@ -3,11 +3,13 @@
 TxOut::TxOut   (std::string address, double amount):
                 address_{address}, amount_{amount}{}
 
-std::string TxOut:: GetVal(){
-    std::stringstream ss;
-    ss << address_ << amount_;
-    return ss.str();
-}
+//todo: -hugo
+//name GetVal appropriately
+//std::string TxOut:: GetVal(){
+//    std::stringstream ss;
+//    ss << address_ << amount_;
+//    return ss.str();
+//}
 
 std::string TxOut:: GetAddress(){
     return address_;
