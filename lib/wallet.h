7ffa22ada2d0dc5d7972886bfd13a5c455d63ffb
hugo001 <contacthugo1@gmail.com>
2018-04-29 10:00:46 -0400

    Wallet restructure: -createtransaction() -> send rename and further interface abstraction -shift from validating CC address to validating with RawKP -added private helper methods to increase createtransaction granularity

--- lib/wallet.h
+++ lib/wallet.h
@@ -12,8 +12,6 @@
 #include "transaction.h"
 #include "utxoutpool.h"
 
-
-
 /*
  * AddressFormat:
  * 4 char prefix[ CC (for capcoin) + XX (place for future stuff, default 00) ] + address + 4 char checksum
@@ -31,22 +29,28 @@ class Wallet{
 public:
 
     Wallet();
-    Wallet(UnspentTxOutPool& UTXO);
-    Transaction* createTransaction(std::vector<TxIn>& tx_inputs, std::vector<TxOut>& tx_outputs);
-    std::vector<std::pair<std::string, double> > getWalletBalance();
+    Wallet(UnspentTxOutPool* UTXO);
+    void send(double ccAmt, std::string toCCAddresses);
+    std::vector<std::pair<std::string, double> >* getWalletBalance();
     void createAddress(int quantity);
 
-    bool isEmpty();
+    bool isWalletActive();
     //void Wallet::shutdownWallet();
 
 private:
 
-    bool empty;
+    bool valid;
+    Transaction* createTransaction(std::string& ccAddress, double& ccAmt);
     void initAddresses(int quantity=0);
     void makeKeyPairs(int quantity=0);
     void createWallet();
     void initWallet();
-    void validateAddresses();
+    void validateRawAddresses();
+
+    int gatherUnspentOutputs(double& ccAmt, std::vector<TxOut>& vtxOut, double& unspentBal);
+
+    void setTxInput(std::vector<TxIn> &txinputs, std::vector<TxOut> &txoutput);
+    int setTxOutput(std::vector<TxOut> &txoutputs, std::string& ccAddress, double& ccAmt, double& unspentBal);
 
     void updateWalletBalance();
     void writeWalletToDisk();
