fdc558bc0b0d61f0c15bd11e0bda1c0b5b5a1e15
Jkb6873 <33443652+Jkb6873@users.noreply.github.com>
2018-05-08 01:35:35 -0400

    Create Miner basics

---
+++ lib/miner.h
@@ -0,0 +1,18 @@
+#ifndef MINER_H
+#define MINER_H
+
+#include "transactionpool.h"
+
+class Miner{
+public:
+  //Miner takes a blockchain to add blocks onto, as well as an address to reward coins to
+  Miner(Blockchain* chain, std::string address);
+  
+  //To mine, we pass a vector of transactions 
+  mine(TransactionPool& pool);
+private:
+  Blockchain* chain_;
+  std::string address_;
+};
+
+#endif
