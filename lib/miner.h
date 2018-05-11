d76ec7905bc10187b8f00a802be0250e7df96e62
calvinqh <calvinquach@gmail.com>
2018-05-11 10:23:24 -0400

    Create mine loop for threading, add comments to headers and src.

--- lib/miner.h
+++ lib/miner.h
@@ -9,11 +9,35 @@ public:
   //Miner takes a blockchain to add blocks onto, as well as an address to reward coins to
   Miner(Blockchain* chain, std::string address);
 
-  //To mine, we pass a vector of transactions
   void mine(bool& killMiner, TransactionPool& pool);
+  
+  /**
+   *
+   * @brief             A routine for mining blocks, designed to be used in a thread.
+   * @details           First the routine will create a coinbase transaction for the miner
+   *                    Then it will package transactions from the transaction pool for the
+   *                    block. The packaging routine will be interrupted when a signal to
+   *                    kill the miner is activated.
+   *                    If there was no interruption, the packaged transactions will be handed
+   *                    off to perform the mining operation on the new block. Along with the
+   *                    transactions, the signal is passed down to the mining operation. The
+   *                    purpose of this is to cancel the mining operation when a signal to
+   *                    stop (killMiner) mining is activated.
+   *
+   * @param killMiner   The signal that will be modified by other processes. This signal is
+   *                    used to tell kill mining operation.
+   *
+   * @param pool        A reference to the transaction pool, where the miner will draw
+   *                    transactions from. And package the transaction into the mined block
+   *
+   */
+  void mine_loop(bool& killMiner, TransactionPool& pool);
 
 private:
+  //A reference to the blockchain
   Blockchain* chain_;
+
+  //The miners address, where the reward is sent to.
   std::string address_;
 };
 
--- src/miner.cpp
+++ src/miner.cpp
@@ -9,15 +9,18 @@ void Miner::mine(bool& killMiner, TransactionPool& pool){
   std::vector<TxIn> ins{coinbaseIn};
   std::vector<TxOut> outs{coinbaseOut};
   Transaction coinbaseTx(ins, outs);
+
+  //Collection of transactions that will be placed in the mined block
   std::vector<Transaction> txSupply{coinbaseTx};
 
   //start a timer
   time_t start = time(0);
+
+  //Continue mining until there is a signal to kill the miner
   while(!killMiner){
     //if > 20 txns in pool, package 20 and generate a block
     //if 20 > n > 0 txns in pool, and 200 seconds pass, package however many there are into a block.
     int size = pool.size() > 20 ? 20 : pool.size();
-
     if(size == 20 || (time(0)-5 > start && size > 0)){
       for(int i = 0; i < size; i++){
         txSupply.push_back(pool.front());
@@ -26,11 +29,56 @@ void Miner::mine(bool& killMiner, TransactionPool& pool){
       chain_->GenerateNextBlock(killMiner, txSupply);
     }
   }
+  //If there is a signal to kill the miner, put transactions back into pool
   if (killMiner){
-    for (int i = size; i > 1; i--){
+    for (int i = txSupply.size(); i > 1; i--){
       pool.push(txSupply[i]);
       txSupply.pop_back();
     }
   }
 }
 
+void Miner::mine_loop(bool& killMiner, TransactionPool& pool) {
+
+    while(true) {
+        //Create a coinbase transaction for the miner, aka the reward
+        TxIn coninbaseIn("","",chain->GetHeight());
+        TxOut coinbaseOut(address_, 50);
+        std::vector<TxIn> ins{coinbaseIn};
+        std::vector<TxOut> outs{coinbaseOut};
+        Transaction coinbaseTx{ins,outs};
+
+        //Collection of transactions that will be placed in the mined block
+        std::vector<Transaction> txSupply{coinbaseTx};
+
+        //start a timer
+        time_t start = time(0);
+
+        //This loop packages transaction for the block
+        //When packaging is completed, mining will commence
+        //Loop runs when the signal to kill miner is off.
+        while(!killMiner){
+            //if > 20 txns in pool, package 20 and generate a block
+            //if 20 > n > 0 txns in pool, and 200 seconds pass, package 
+            //however many there are into a block.
+            int size = pool.size() > 20 ? 20 : pool.size();
+            if(size == 20 || (time(0)-5 > start && size > 0)){
+                  //Package transactions into txSupply
+                  for(int i = 0; i < size; i++){
+                    txSupply.push_back(pool.front());
+                    pool.pop();
+                  }
+                  // Mine the block
+                  chain_->GenerateNextBlock(killMiner, txSupply);
+            }
+        }
+        //If there was a signal to kill the miner, put packaged transaction back into pool
+        if(killMiner) {
+            for(int i = txSupply.size(); i > 1; i--) {
+                pool.push(txSupply[i]);
+                txSupply.pop_back();
+            }
+        }
+    }
+
+}
