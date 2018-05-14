43f17948f69ae7d5b3a0b084eb7d1dcd994598b2
calvinqh <calvinquach@gmail.com>
2018-05-13 21:59:04 -0400

    Update reject block message header.

--- src/network.cpp
+++ src/network.cpp
@@ -162,14 +162,14 @@ void Network::listen(){
         //Push transaction into pool
         bool result = txpool_->push(newTx);
       }
-      else if(s.substr(1,12) == "BLOCK_REJECT") {
+      else if(s.substr(1,12) == "REJECT_BLOCK") {
         //The broadcasted block was rejected by server
         std::cout << "[network]: Block was rejected!" << std::endl;
        
         //Drop all blocks up to server blockchain height 
         std::cout << "[network]: \"" << s.substr(16,1) << "\"" <<std::endl;
-        size_t index = stol(s.substr(16,1));
-        std::cout << "[network]: Server last block index: " << index << std::endl;
+        //size_t index = stol(s.substr(16,1));
+        std::cout << "[network]: Server last block index: " <<  std::endl;
       }
       if(s.substr(0, 3) == "END")
       {
@@ -352,7 +352,7 @@ void Network::runServer() {
                     std::cout << "[network]: Rejected block" << std::endl;
                     //Tell the socket that sent this block, Server rejected
                     std::cout << "[network]: Tell socket" << sd << " block rejected" << std::endl; 
-                    std::string response = "\"BLOCK_REJECT\":\"";
+                    std::string response = "\"REJECT_BLOCK\":\"";
                     response += std::to_string(blockchain->GetLastBlock().GetIndex());
                     response += "\"";
                     std::cout << "[network]: Sending: " << response << std::endl;
